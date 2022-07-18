eb908d4d61c4f6744b739ead6a341d046bd9a6e0
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-07-18 18:19:12 +0000

    have another tweet by descartes, some styling changes made

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#tweet-text").keyup(function () {
-    let characterLength = $(this).val().length
     let counterValue = 140
+    let characterLength = $(this).val().length
     let presentValue = counterValue - characterLength
     $('.counter').text(presentValue).toggleClass('warning', presentValue < 0)
    
