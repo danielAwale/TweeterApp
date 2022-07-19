379c9109994d648c3b47f73eb4e0602b690a1029
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-07-19 23:35:48 +0000

    have the ability to render the html dynamically through the added functions in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,65 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(() => {
+  renderTweets(data);
+});
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+};
+
+const createTweetElement = function (tweet) {
+  return `
+    <article>
+    <header class="makingtweets">
+      <div class="tweet-username">
+        <img src=${tweet.user.avatars}>
+        <span>${tweet.user.name}</span>
+        <span>${tweet.user.handle}</span>
+      </div>
+      <p class="thetweet"><b>${tweet.content.text}</b></p>
+    </header>
+    <footer class="footer-tweet">
+      <span>${tweet.created_at}</span>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`
+};
+
+
+
+
 
