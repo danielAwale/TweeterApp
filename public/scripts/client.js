1965cf7c8f08926c8567944ac64d82b142d7fe28
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-07-21 05:41:43 +0000

    render tweets through ajax get annd post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,41 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-$(document).ready(() => {
-  renderTweets(data);
-});
-
-const renderTweets = function (tweets) {
-  for (let tweet of tweets) {
-    $('#tweets-container').append(createTweetElement(tweet));
-  }
-};
 
 const createTweetElement = function (tweet) {
   return `
@@ -61,7 +26,36 @@ const createTweetElement = function (tweet) {
   </article>`
 };
 
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $('#tweets-container').prepend(createTweetElement(tweet));
+  }
+};
 
+const loadTweets = function () {
+  $.ajax('/tweets', {
+    type: 'GET',
+  }).then(function (tweet) {
+    console.log(tweet);
+    renderTweets(tweet);
+  })
+};
 
+const submitTweets = function () {
+  $("#thisform").on('submit', function (event) {
+    event.preventDefault();
+    $.ajax('/tweets', {
+      type: "POST",
+      data: $(this).serialize()
+    }).then(function (res) {
+      $('#tweets-container').empty();
+      loadTweets();
+    })
+  });
+}
 
+$(document).ready(() => {
+  loadTweets();
+  submitTweets();
+});
 
