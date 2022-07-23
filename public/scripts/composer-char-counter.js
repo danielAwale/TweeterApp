bfec4811d2dddf5e0852ab2999510c5ee9f5f9c5
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-07-23 01:46:34 +0000

    have the counter turn red if the count is below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
   $("#tweet-text").keyup(function () {
-    let counterValue = 140
+    let counterOneForty = 140
     let characterLength = $(this).val().length
-    let presentValue = counterValue - characterLength
-    $('.counter').text(presentValue).toggleClass('warning', presentValue < 0)
+    let presentValue = counterOneForty - characterLength
+    $('.counter').text(presentValue).toggleClass('color-change', presentValue < 0);
+  });
 
-  })
-})
+  $('.counter').css({
+    'color': color
+  });
+}
+)
